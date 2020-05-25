0e14dc5a2d36f4de788734b8678cbc2f94bc492d
Pebanh <peter.banh95@gmail.com>
2020-05-24 23:59:32 -0400

    Add client side functions to have button in the index page call the googlescript API. Fix error/bugs in general functions

--- APIs.js
+++ APIs.js
@@ -1,31 +1,38 @@
 /*------------ API functions for Client Side ------------ */
-function dataRetrieveFor(urlCode="abc") {
+function dataRetrieveFor(urlCode = "abc") {
   var data = dataSheet.getDataRange().getValues();
   var formResponseData = formResponseSheet.getDataRange().getValues();
   const URL_ARR_INDEX = 2;
   const ROW_ARR_INDEX = 3;
-  var rowIndexes = data[findRowInCol(URL_ARR_INDEX,urlCode,data)][ROW_ARR_INDEX].split(',');
+  var rowIndexes = data[findRowInCol(URL_ARR_INDEX, urlCode, data)][
+    ROW_ARR_INDEX
+  ].split(",");
   console.log(rowIndexes);
   var dataArr = [];
-  for(let i =0; i<rowIndexes.length; i++){
+  for (let i = 0; i < rowIndexes.length; i++) {
     let tempObj = {};
-    for(let j=0; j<formResponseData[0].length; j++){
-      tempObj[j] = formResponseData[parseInt(rowIndexes[i])-1][j]
+    for (let j = 0; j < formResponseData[0].length; j++) {
+      tempObj[j] = formResponseData[parseInt(rowIndexes[i]) - 1][j];
     }
     dataArr.push(tempObj);
   }
-  return dataArr
+  return dataArr;
 }
 
-function urlGen(email = "Annasitu8@gmail.com"){
+function urlGen(email = "Annasitu8@gmail.com") {
   var data = dataSheet.getDataRange().getValues();
   var formResponseData = formResponseSheet.getDataRange().getValues();
   const EMAIL_ARR_INDEX = 1;
   const URL_ARR_INDEX = 2;
-  if(findRowInCol(EMAIL_ARR_INDEX, email, data)){
+  if (findRowInCol(EMAIL_ARR_INDEX, email.toLowerCase(), data)) {
     var url = randChars();
-    dataSheet.getRange(findRowInCol(EMAIL_ARR_INDEX, email, data)+1, URL_ARR_INDEX+1).setValue(url);
-    return "Email found, user will receieve email with URL to view Google Form Sub"
+    dataSheet
+      .getRange(
+        findRowInCol(EMAIL_ARR_INDEX, email, data) + 1,
+        URL_ARR_INDEX + 1
+      )
+      .setValue(url);
+    return "Email found, user will receieve email with URL to view Google Form Sub";
   }
-  return "No email found"
+  return "No email found";
 }
--- Main.js
+++ Main.js
@@ -1,58 +1,59 @@
- var formResponseSheet = SpreadsheetApp.getActiveSpreadsheet().getSheetByName('Form Responses 1');
- var dataSheet = SpreadsheetApp.getActiveSpreadsheet().getSheetByName('Data');
+var formResponseSheet = SpreadsheetApp.getActiveSpreadsheet().getSheetByName(
+  "Form Responses 1"
+);
+var dataSheet = SpreadsheetApp.getActiveSpreadsheet().getSheetByName("Data");
 
 /*------------ General Functions ------------ */
-function findColumnInRow(row, string, arr){
+function findColumnInRow(row, string, arr) {
   let colIndex = 0;
-  for(let i=0; i<arr[0].length; i++){
-    if(arr[row][i].include(string)){
-      colIndex=i;
+  for (let i = 0; i < arr[0].length; i++) {
+    if (arr[row][i].toLowerCase().includes(string)) {
+      colIndex = i;
       return colIndex;
     }
   }
   return colIndex;
 }
 
-
-function findRowInCol(col, string, arr){
+function findRowInCol(col, string, arr) {
   let rowIndex = 0;
-  for(let i=0; i<arr.length; i++){
+  for (let i = 0; i < arr.length; i++) {
     Logger.log(arr[i][col]);
-    if(arr[i][col].include(string)){
-      rowIndex=i;
-      return rowIndex
+    if (arr[i][col].toLowerCase().includes(string)) {
+      rowIndex = i;
+      return rowIndex;
     }
   }
   return rowIndex;
 }
 
-function randChars(){
+function randChars() {
   var url = "";
   var randNum = 0;
-  for(let i=0; i<16; i++){
-    randNum = Math.floor(48+Math.random()*75);
-    while((randNum > 57 && randNum < 65) || (randNum > 90 && randNum < 97)){
-      randNum = Math.floor(48+Math.random()*75)
-     }
-    url += String.fromCharCode(randNum)
+  for (let i = 0; i < 16; i++) {
+    randNum = Math.floor(48 + Math.random() * 75);
+    while ((randNum > 57 && randNum < 65) || (randNum > 90 && randNum < 97)) {
+      randNum = Math.floor(48 + Math.random() * 75);
+    }
+    url += String.fromCharCode(randNum);
   }
-  return url
+  return url;
 }
 
 /*------------ Web Apps Functions ------------ */
 
 function doGet(e) {
   // This function allows us to use the Google Web App. Essentially you call up the html file.
-  if(e.parameter['urlCode']){
-    var htmltemp = HtmlService.createTemplateFromFile('userdata');
+  if (e.parameter["urlCode"]) {
+    var htmltemp = HtmlService.createTemplateFromFile("userdata");
     return htmltemp.evaluate();
-  }else{
-    var htmltemp = HtmlService.createTemplateFromFile('index');
+  } else {
+    var htmltemp = HtmlService.createTemplateFromFile("index");
     return htmltemp.evaluate();
   }
 }
 
-function include(filename){
+function include(filename) {
   // Function to allow HTML files to connect with one another
-  return HtmlService.createHtmlOutputFromFile(filename).getContent();  
+  return HtmlService.createHtmlOutputFromFile(filename).getContent();
 }
